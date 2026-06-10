9051bcee77f6616798272be233ee7a0d99fd87bf
Vinícius Barros da Silva <vinicius.barros@unesp.br>
2026-06-10 14:27:13 +0000

    teste_primeiros_passos

--- models/my_first_dbt_model.sql
+++ models/my_first_dbt_model.sql
@@ -3,7 +3,7 @@
     Welcome to your first dbt model!
     Did you know that you can also configure models directly within SQL files?
     This will override configurations stated in dbt_project.yml
-
+--just some tune up --06/10/2026 --Vinícius Barros
     Try changing "table" to "view" below
 */
 
